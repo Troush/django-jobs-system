6ba9378da31fb0acba89ec1683b7bc802d14a286
troush <troush69@gmail.com>
2013-04-13 14:27:28 +0300

    Created app named CORE(MAIN APP OF Project), added static files (Bootstrap - Flat-ui)

--- job/urls.py
+++ job/urls.py
@@ -1,17 +1,20 @@
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
+
+from core import views
+
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'job.views.home', name='home'),
     # url(r'^job/', include('job.foo.urls')),
-
+    url(r'^$', views.index, name='index'),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
